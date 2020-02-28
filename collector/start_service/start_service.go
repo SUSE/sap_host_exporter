524c3925c5099b25e0f7af7d4bafd34b05361282
dmaiocchi <dmaiocchi@suse.com>
2020-02-28 13:10:29 +0100

    Add sketron of new startService collector

---
+++ collector/start_service/start_service.go
@@ -0,0 +1,28 @@
+package start_service
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/SUSE/sap_host_exporter/collector"
+)
+
+func NewCollector() (*soapCollector, error) {
+
+	c := &soapCollector{
+		collector.NewDefaultCollector("soap"),
+	}
+
+	c.SetDescriptor("process", "get status of the Process running on the host", []string{"status"})
+
+	return c, nil
+}
+
+type soapCollector struct {
+	collector.DefaultCollector
+}
+
+func (c *soapCollector) Collect(ch chan<- prometheus.Metric) {
+	log.Infoln("Collecting SOAP metrics")
+
+}
