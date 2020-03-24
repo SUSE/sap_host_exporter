a1038995a0d65eb399e4a54efae6f92541a8c77e
Stefano Torresi <stefano@torresi.io>
2020-03-24 12:41:19 +0100

    add authentication with github api

--- .ci/gh_release_to_obs_changeset.py
+++ .ci/gh_release_to_obs_changeset.py
@@ -2,9 +2,10 @@
 
 import argparse
 import json
+import os
 import sys
 import textwrap
-from urllib import request
+import urllib.request
 import urllib.error
 from datetime import datetime
 from datetime import timezone
@@ -25,8 +26,14 @@ args = parser.parse_args()
 releaseSegment = f"/tags/{args.tag}" if args.tag else "/latest"
 url = f'https://api.github.com/repos/{args.repo}/releases{releaseSegment}'
 
+request = urllib.request.Request(url)
+
+githubToken = os.getenv("GITHUB_OAUTH_TOKEN")
+if githubToken:
+    request.add_header("Authorization", "token " + githubToken)
+
 try:
-    response = request.urlopen(url)
+    response = urllib.request.urlopen(request)
 except urllib.error.HTTPError as error:
     if error.code == 404:
         print(f"Release {args.tag} not found in {args.repo}. Skipping changelog generation.")
