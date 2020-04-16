7133972d13651fc147e1e08abcf09c8263790d58
dmaiocchi <dmaiocchi@suse.com>
2020-04-16 15:15:08 +0200

    implement unit-test

--- collector/collector_register/register.go
+++ collector/collector_register/register.go
@@ -6,7 +6,7 @@ import (
 	"github.com/SUSE/sap_host_exporter/internal/sapcontrol"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
-	 log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 // RegisterOptionalCollectors register depending on the system where the exporter run the additional collectors
@@ -19,20 +19,17 @@ func RegisterOptionalCollectors(webService sapcontrol.WebService) error {
 	}
 
 	for _, process := range processList.Processes {
-		// if we found msg_server on process name we register the Enqueue Server
 		if process.Name == "msg_server" {
 			enqueueFound = true
 		}
-		// if we found disp+work on process name we register the dispatcher collector
 		if process.Name == "disp+work" {
 			dispatcherFound = true
 		}
-
 		if enqueueFound == true && dispatcherFound == true {
 			break
 		}
 	}
-
+	// if we found msg_server on process name we register the Enqueue Server
 	if enqueueFound == true {
 		enqueueServerCollector, err := enqueue_server.NewCollector(webService)
 		if err != nil {
@@ -42,6 +39,7 @@ func RegisterOptionalCollectors(webService sapcontrol.WebService) error {
 			log.Info("Enqueue Server optional collector registered")
 		}
 	}
+	// if we found disp+work on process name we register the dispatcher collector
 	if dispatcherFound == true {
 		dispatcherCollector, err := dispatcher.NewCollector(webService)
 		if err != nil {
